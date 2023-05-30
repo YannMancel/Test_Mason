ca9d6c222c6e857fc26d160005a590916abee235
yann_mancel <yann.mancel@hotmail.fr>
2023-05-30 15:24:15 +0200

    ✨ feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test_Mason
+**Goal**: project to test mason.
+
+## Setup the project
+Download the project code, preferably using `git clone git@github.com:YannMancel/Test_Mason.git`.
---
+++ bricks/directory_generator/__brick__/{{directory_name.snakeCase()}}/_{{directory_name.camelCase()}}.dart
@@ -0,0 +1,3 @@
+{{#has_logic}}export 'logics/_logics.dart';{{/has_logic}}
+{{#has_view}}export 'views/_views.dart';{{/has_view}}
+{{#has_model}}export 'models/_models.dart';{{/has_model}}
